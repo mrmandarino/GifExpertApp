ee0cb7f92f9a2eb08e1e442d9195ad4f9de953cb
René Avendaño <samuel.benicio.delmar@gmail.com>
2023-02-22 03:21:42 -0300

    se agregó una condicional ternaria para determinar si un gif tiene titulo o no y mostrar un texto alternativo en su lugar.

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -5,7 +5,7 @@ export const GifItem = ({id, title, url}) => {
     return (
       <div className="card">
         <img src={url} alt={title}/>
-        <p>{title}</p>
+        {title!=" "?<p>{title}</p> : <p>No title</p>}
       </div>
     )
 }
